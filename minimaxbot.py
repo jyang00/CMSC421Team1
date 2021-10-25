5ed92b4395e5f33f09a01038433b2b54bce6e3b8
Michael Tang <mtang124@terpmail.umd.edu>
2021-10-25 18:30:55 -0400

    skeleton code

---
+++ minimaxbot.py
@@ -0,0 +1,8 @@
+class minimaxbot:
+
+    def __init__(self):
+        pass
+        
+    def getNextMove(self, gameBoard):
+        # Do some fancy things with this.gameBoard
+        pass
---
+++ tictactoe.py
@@ -0,0 +1,8 @@
+
+class tictactoe:
+    
+    def __init__(self):
+        self.gameBoard = [] # x6 xD
+
+    def updateGame(self):
+        pass
