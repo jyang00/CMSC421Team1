8730b07a95520dbcf214962f32107be0a43e1cdc
acneumyer <aneumyer@umd.edu>
2021-11-22 23:42:31 -0500

    Added comments after testing different depths at different points in the game

--- src/usefulTester.py
+++ src/usefulTester.py
@@ -14,14 +14,29 @@ game = cT.CubicTicTacToe()
 
 ## Here are some sample moves to get the game started 
 # (the X moves were decided by a depth=4 bot)
-# game.make_move('X', "top", 0)
-# game.make_move('O', "right", 5)
-# game.make_move('X', "left", 6)
-# game.make_move('O', "bottom", 3)
-# game.make_move('X', "top", 1)
-# game.make_move('O', "bottom", 4)
-# game.make_move('X', "top", 4)
-# game.make_move('O', "right", 4)
+game.make_move('X', "top", 0)
+game.make_move('O', "right", 5)
+game.make_move('X', "left", 6)
+game.make_move('O', "bottom", 3)
+game.make_move('X', "top", 1)
+game.make_move('O', "bottom", 4)
+game.make_move('X', "top", 4)
+game.make_move('O', "right", 4)
+## Above is 8 moves in
+game.make_move('X', "right", 3)
+game.make_move('O', "back", 5)
+game.make_move('X', "bottom", 0)
+game.make_move('O', "back", 4)
+## Above is 12 moves in
+game.make_move('X', "back", 7)
+game.make_move('O', "back", 6)
+game.make_move('X', "top", 2)
+game.make_move('O', "top", 3)
+## Above is 16 moves in
+#game.make_move('X', "top", 5)
+#game.make_move('O', "top", 6)
+
+game.display_boards()
 
 end_time_1 = tI.perf_counter_ns()
 print(f"Play Moves Time: {end_time_1 - start_time}")
@@ -34,7 +49,7 @@ bot = mM.MinimaxBot('X', 'O', 1, 1)
 
 # Depth = 4 means 3 moves, as the depth refers to how many levels of the tree
 # there are and each move passes between levels
-(side, spot) = bot.calculateTree(game, 4)
+(side, spot) = bot.calculateTree(game, 7)
 game.make_move('X', side, spot)
 
 # Print out the bot's move to see what it chose
@@ -51,9 +66,14 @@ print(f"Calculate Tree Time (ns): {end_time_2 - end_time_1}")
 game.display_game()
 print(f'X-score: {game.x_score}, O-score: {game.o_score}')
 
+## Depth 5 starts out around 45s, but drops to like 6 seconds 8 moves in, so I think we can use it
 
+## Depth 6 gets down to like 50s 12 moves in, so we should really only use it
+## like halfway into the game. It drops to 20ish seconds (large variation) 2 moves later (14 total)
+## It drops to 8s 16 moves in
+## It drops to 2s 18 moves in
 
-
+## Depth 7 gets to 8 seconds 18 moves in, 15s 17 moves in, 37s 16 moves in
 
 
 
